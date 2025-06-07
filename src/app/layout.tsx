cb8eb565795f3826a23a9d373b1b7665bd9da77e
Mandy9943 <armandoc9943@gmail.com>
2025-06-07 15:44:57 -0300

    Update favicon and enhance metadata in layout.tsx with Open Graph and Twitter card details for improved SEO and social sharing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,33 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Lock Savers Locksmiths - 24/7 Emergency Locksmith in Birmingham",
   description:
-    "Your trusted 24/7 locksmith in Birmingham. Fast 20-30 minute arrival, damage-free entry, and transparent pricing. Call 0121 861 8069 for immediate emergency locksmith services.",
+    "Your trusted 24/7 locksmith in Birmingham. Fast 20-30 minute arrival, damage-free entry, and transparent pricing. Call +44 7551 458842 for immediate emergency locksmith services.",
+  openGraph: {
+    url: "https://locksaverslocksmiths.com",
+    type: "website",
+    title: "Lock Savers Locksmiths - 24/7 Emergency Locksmith in Birmingham",
+    description:
+      "Your trusted 24/7 locksmith in Birmingham. Fast 20-30 minute arrival, damage-free entry, and transparent pricing. Call +44 7551 458842 for immediate emergency locksmith services.",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/a611f3ba-e0cd-4276-9237-153cabf1d807.png?token=LhsIsjwkwInpRO8Dzs0BPqHoe5JHvfSIp-f3VQbGlMk&height=596&width=1200&expires=33285321625",
+        width: 1200,
+        height: 596,
+        alt: "Lock Savers Locksmiths - 24/7 Emergency Locksmith in Birmingham",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    domain: "locksaverslocksmiths.com",
+    url: "https://locksaverslocksmiths.com",
+    title: "Lock Savers Locksmiths - 24/7 Emergency Locksmith in Birmingham",
+    description:
+      "Your trusted 24/7 locksmith in Birmingham. Fast 20-30 minute arrival, damage-free entry, and transparent pricing. Call +44 7551 458842 for immediate emergency locksmith services.",
+    images: [
+      "https://opengraph.b-cdn.net/production/images/a611f3ba-e0cd-4276-9237-153cabf1d807.png?token=LhsIsjwkwInpRO8Dzs0BPqHoe5JHvfSIp-f3VQbGlMk&height=596&width=1200&expires=33285321625",
+    ],
+  },
 };
 
 export default function RootLayout({
